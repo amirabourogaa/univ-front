4c8876a41a25b80c605060ff31cb09aa618b1649
amirabourogaa <abouragba@u-label.com>
2025-01-03 10:25:26 +0100

    Fix order and S

--- src/components/products/data.js
+++ src/components/products/data.js
@@ -1,4 +1,83 @@
 export const categories = [
+  {
+    name: "Pneumatique",
+    image: '/products/pneumatique/pneumatique.jpg',
+    subcategories: [
+      { 
+        name: "Compresseurs", 
+        image: "/products/pneumatique/compresseur/compresseur.jpg" 
+      },
+      { 
+        name: "Traitement de l'air", 
+        image: "/products/pneumatique/traitement-de-lair/traitement-de-lair.jpg" 
+      },
+      { 
+        name: "Distributeurs", 
+        image: "/products/pneumatique/distributeur/distributeur.jpg" 
+      },
+      { 
+        name: "Actionneurs", 
+        image: "/products/pneumatique/actionneur/actionneur.jpg" 
+      },
+     
+     
+      { 
+        name: "Raccords et tubes", 
+        image: "/products/pneumatique/raccord-et-tube/raccord-et-tube.jpg" 
+      },
+     
+    ],
+  },
+  {
+    name: "Technique de vide",
+    image: '/products/technique-de-vide/technique-de-vide.jpg',
+    subcategories: [
+      { 
+        name: "Pompes à vides", 
+        image: "/products/technique-de-vide/POMPES/POMPES.jpg" 
+      },
+      { 
+        name: "Generateurs de vide et venturi", 
+        image: "/products/technique-de-vide/generateur/generateur.jpg" 
+      },
+      { 
+        name: "Ventouses et porte ventouses", 
+        image: "/products/technique-de-vide/ventouses/ventouses.jpg" 
+      },
+      { 
+        name: "Instrumentation et controle de vide", 
+        image: "/products/technique-de-vide/instrumentation/instrumentation.png" 
+      },
+     
+      { 
+        name: "Filtres pour vide", 
+        image: "/products/technique-de-vide/filtres-pour-vide/filtres-pour-vide.png" 
+      },
+    
+     
+ 
+    ],
+  },
+  {
+    name: "Instrumentation et regulation",
+    image: '/products/instrumentation-et-regulation/instrumentation-et-regulation.jpg',
+    subcategories: [
+      { 
+        name: "Capteurs", 
+        image: "/products/instrumentation-et-regulation/capteurs/capteurs.jpg" 
+      },
+      { 
+        name: "Transmetteurs", 
+        image: "/products/instrumentation-et-regulation/transmetteurs/transmetteurs.jpg" 
+      },
+      { 
+        name: "Controleurs", 
+        image: "/products/instrumentation-et-regulation/controleurs/controleurs.jpg" 
+      },
+     
+    ],
+  },
+
     {
       name: "Automatisme",
       image: '/products/automatisme/auto.jpg',
@@ -8,6 +87,18 @@ export const categories = [
           image: "/products/automatisme/actionneur-electrique/actionneur.jpg" 
         },
         { 
+          name: "Contrôle d'electricite statique", 
+          image: "/products/automatisme/controle/controle.jpg" 
+        },
+        { 
+          name: "HMI ET PLC", 
+          image: "/products/automatisme/hmi-et-plc/hmi.jpg" 
+        },
+        { 
+          name: "Detection", 
+          image: "/products/automatisme/detection/detection.png" 
+        },
+        { 
           name: "Commande de moteur electrique", 
           image: "/products/automatisme/commande/commande.jpg" 
         },
@@ -15,28 +106,15 @@ export const categories = [
           name: "Composants electrique", 
           image: "/products/automatisme/composants/composants.jpg" 
         },
-        { 
-          name: "Contrôle d'electricite statique", 
-          image: "/products/automatisme/controle/controle.jpg" 
-        },
-        { 
-          name: "Detection", 
-          image: "/products/automatisme/detection/detection.png" 
-        },
-        { 
-          name: "Hmi et plc", 
-          image: "/products/automatisme/hmi-et-plc/hmi.jpg" 
-        },
+       
+        
+       
       ],
     },
     {
       name: "Controle de fluide",
       image: '/products/controle-de-fluide/controle.jpg',
       subcategories: [
-        { 
-          name: "Electrovannes", 
-          image: "/products/controle-de-fluide/ELECTROVANNES/ELECTROVANNES.png" 
-        },
         { 
           name: "Vannes de process", 
           image: "/products/controle-de-fluide/VANNES-DE-PROCESS/VANNES-DE-PROCESS.jpg" 
@@ -45,6 +123,12 @@ export const categories = [
           name: "Vannes de regulation", 
           image: "/products/controle-de-fluide/VANNES-DE-REGULATION/VANNES-DE-REGULATION.jpg" 
         },
+       
+        { 
+          name: "Electrovannes", 
+          image: "/products/controle-de-fluide/ELECTROVANNES/ELECTROVANNES.png" 
+        },
+       
       ],
     },
     {
@@ -52,92 +136,26 @@ export const categories = [
       image: '/products/hydraulique/hydraulique.jpg',
       subcategories: [
         { 
-          name: "Central hydraulique", 
+          name: "Centrals hydraulique", 
           image: "/products/hydraulique/CENTRAL-HYDRAULIQUE/CENTRAL-HYDRAULIQUE.png" 
         },
         { 
-          name: "Distributeur hydraulique", 
+          name: "Distributeurs hydraulique", 
           image: "/products/hydraulique/distributeur-hydraulique/distributeur-hydraulique.jpg" 
         },
         { 
-          name: "Verin hydraulique", 
-          image: "/products/hydraulique/verin-hydraulique/verin-hydraulique.jpeg" 
-        },
-        { 
-          name: "Pompe hydraulique", 
+          name: "Pompes hydraulique", 
           image: "/products/hydraulique/pompe-hydraulique/pompe-hydraulique.jpg" 
         },
+        { 
+          name: "Verins hydraulique", 
+          image: "/products/hydraulique/verin-hydraulique/verin-hydraulique.jpeg" 
+        },
+       
       ],
     },
-    {
-      name: "Instrumentation et regulation",
-      image: '/products/instrumentation-et-regulation/instrumentation-et-regulation.jpg',
-      subcategories: [
-        { 
-          name: "Capteurs", 
-          image: "/products/instrumentation-et-regulation/capteurs/capteurs.jpg" 
-        },
-        { 
-          name: "Controleurs", 
-          image: "/products/instrumentation-et-regulation/controleurs/controleurs.jpg" 
-        },
-        { 
-          name: "Transmetteurs", 
-          image: "/products/instrumentation-et-regulation/transmetteurs/transmetteurs.jpg" 
-        }
-      ],
-    },
-    {
-      name: "Pneumatique",
-      image: '/products/pneumatique/pneumatique.jpg',
-      subcategories: [
-        { 
-          name: "Actionneur", 
-          image: "/products/pneumatique/actionneur/actionneur.jpg" 
-        },
-        { 
-          name: "Compresseur", 
-          image: "/products/pneumatique/compresseur/compresseur.jpg" 
-        },
-        { 
-          name: "Distributeur", 
-          image: "/products/pneumatique/distributeur/distributeur.jpg" 
-        },
-        { 
-          name: "Raccord et tube", 
-          image: "/products/pneumatique/raccord-et-tube/raccord-et-tube.jpg" 
-        },
-        { 
-          name: "Traitement de l'air", 
-          image: "/products/pneumatique/traitement-de-lair/traitement-de-lair.jpg" 
-        }
-      ],
-    },
-    {
-      name: "Technique de vide",
-      image: '/products/technique-de-vide/technique-de-vide.jpg',
-      subcategories: [
-        { 
-          name: "Filtres pour vide", 
-          image: "/products/technique-de-vide/filtres-pour-vide/filtres-pour-vide.png" 
-        },
-        { 
-          name: "Generateur de vide et venturi", 
-          image: "/products/technique-de-vide/generateur/generateur.jpg" 
-        },
-        { 
-          name: "Instrumentation et controle de vide", 
-          image: "/products/technique-de-vide/instrumentation/instrumentation.png" 
-        },
-        { 
-          name: "Pompes à vides", 
-          image: "/products/technique-de-vide/POMPES/POMPES.jpg" 
-        },
-        { 
-          name: "Ventouses et porte ventouses", 
-          image: "/products/technique-de-vide/ventouses/ventouses.jpg" 
-        }
-      ],
-    },
+  
+ 
+   
   ];
   
